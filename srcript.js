81b1d5caa9f8ca7f830131a6c59af24d56ef0f5c
lissenko228 <terrariap417@gmail.com>
2023-05-01 22:36:00 +0400

    pupa

---
+++ srcript.js
@@ -0,0 +1,16 @@
+function change1() 
+{
+    document.getElementById('change').src = "assets/img/menu/IMAGE.svg";
+}
+function change2() 
+{
+    document.getElementById('change').src = "assets/img/menu/pic2.jpg";
+}
+function change3() 
+{
+    document.getElementById('change').src = "assets/img/menu/pic3.jpg";
+}
+
+// document.querySelector('menu_a').addEventListener('click', () => {
+//     document.querySelector('menu_a').classList.add('check_a');
+// });
